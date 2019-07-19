3d4d966005e70d4dcfbbc91574b3701d9c681572
User <inoplanee@mail.ru>
2019-07-19 17:55:02 +0600

    new

---
+++ app/src/main/java/mainactivity/musicplayer/example/com/englishzubrila/FirstLevel/CatalogFragmentov/Temp.java
@@ -0,0 +1,17 @@
+package mainactivity.musicplayer.example.com.englishzubrila.FirstLevel.CatalogFragmentov;
+
+class Temp {
+    private int temp;
+
+    public Temp(int temp) {
+        this.temp = temp;
+    }
+
+    public int getTemp(int adapterPosition) {
+        return temp;
+    }
+
+    public void setTemp(int temp) {
+        this.temp = temp;
+    }
+}
