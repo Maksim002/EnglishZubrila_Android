1badbd9a1a438aff8993bb0ca197c1daac9736b8
User <inoplanee@mail.ru>
2019-08-05 17:16:24 +0600

    new

--- app/src/main/java/mainactivity/musicplayer/example/com/englishzubrila/FirstLevel/Elementary/A1Elementary.java
+++ app/src/main/java/mainactivity/musicplayer/example/com/englishzubrila/FirstLevel/Elementary/A1Elementary.java
@@ -16,14 +16,14 @@ public class A1Elementary extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_elementary_amin);
 
-        VideoView videoV = findViewById(R.id.videoView1);
-        String videoPath = "https://www.youtube.com/watch?v=8Lq3HyBCuAA";
-        Uri uri = Uri.parse(videoPath);
-        videoV.setVideoURI(uri);
+        VideoView vidView = (VideoView)findViewById(R.id.videoView);
 
+        String videPath = "android.resource://" + getPackageName() + "/" + R.raw.trans;
+        Uri uri  = Uri.parse(videPath);
+        vidView.setVideoURI(uri);
         MediaController mediaController = new MediaController(this);
-        videoV.setMediaController(mediaController);
-        mediaController.setAnchorView(videoV);
+        vidView.setMediaController(mediaController);
+        mediaController.setAnchorView(vidView);
 
     }
 }
