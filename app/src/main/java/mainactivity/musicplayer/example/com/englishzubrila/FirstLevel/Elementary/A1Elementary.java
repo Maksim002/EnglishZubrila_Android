05ed4ffacf0f8db55a399e72f86d434d4d84763f
User <inoplanee@mail.ru>
2019-08-07 04:04:50 +0600

    new

--- app/src/main/java/mainactivity/musicplayer/example/com/englishzubrila/FirstLevel/Elementary/A1Elementary.java
+++ app/src/main/java/mainactivity/musicplayer/example/com/englishzubrila/FirstLevel/Elementary/A1Elementary.java
@@ -1,11 +1,8 @@
 package mainactivity.musicplayer.example.com.englishzubrila.FirstLevel.Elementary;
 
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.MediaController;
-import android.widget.VideoView;
 
 import mainactivity.musicplayer.example.com.englishzubrila.R;
 
@@ -15,15 +12,6 @@ public class A1Elementary extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_elementary_amin);
-
-        VideoView vidView = (VideoView)findViewById(R.id.videoView);
-
-        String videPath = "android.resource://" + getPackageName() + "/" + R.raw.trans;
-        Uri uri  = Uri.parse(videPath);
-        vidView.setVideoURI(uri);
-        MediaController mediaController = new MediaController(this);
-        vidView.setMediaController(mediaController);
-        mediaController.setAnchorView(vidView);
 
     }
 }
