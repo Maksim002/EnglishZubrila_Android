6039256c4450103c151d581a591cfdbfb01747c9
Maksim002 <inoplanee@Mail.ru>
2019-08-23 15:01:38 +0600

    new

--- app/src/main/java/mainactivity/musicplayer/example/com/englishzubrila/Glav/MainActivity.java
+++ app/src/main/java/mainactivity/musicplayer/example/com/englishzubrila/Glav/MainActivity.java
@@ -1,29 +1,47 @@
 package mainactivity.musicplayer.example.com.englishzubrila.Glav;
 
 import android.content.Intent;
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import mainactivity.musicplayer.example.com.englishzubrila.InitilCourses.InitialFragment;
 import mainactivity.musicplayer.example.com.englishzubrila.R;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView imageScroll;
+    private TextView textZp;
 
-    private Animation animation;
-
-    TextView startText;
-
-    private static int SPLAH_TAME_OUT = 3000;
+    private static int SPLAH_TAME_OUT = 7500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageScroll = findViewById(R.id.scroll);
+        textZp = findViewById(R.id.textZ);
+
+        setImageScroll();
+        setTextZp();
+
+        animateImageView(imageScroll);
+    }
+
+    private void animateImageView(ImageView imageView) {
+        final Drawable drawable = imageView.getDrawable();
+        if (drawable instanceof Animatable) {
+            Animatable animatable = ((Animatable) drawable);
+            animatable.start();
+        }
+    }
+    private void setImageScroll(){
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -32,14 +50,9 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         },SPLAH_TAME_OUT);
-
-        startText = findViewById(R.id.startText);
-
-        fade();
     }
-
-    public void fade(){
-        animation = AnimationUtils.loadAnimation(this,R.anim.fadeout);
-        startText.startAnimation(animation);
+    private void setTextZp(){
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.scale);
+        textZp.startAnimation(animation);
     }
 }
