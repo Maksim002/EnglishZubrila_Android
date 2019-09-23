060c797e0cbc9bce22c8a8e25ff40c0fb4235614
Maksim002 <inoplanee@Mail.ru>
2019-09-23 22:27:30 +0600

    new panidelnik

--- app/src/main/java/mainactivity/musicplayer/example/com/englishzubrila/InitilCourses/ListViewPager.java
+++ app/src/main/java/mainactivity/musicplayer/example/com/englishzubrila/InitilCourses/ListViewPager.java
@@ -49,9 +49,16 @@ public class ListViewPager extends Fragment implements Listener {
         private void getCompanies(){
             list = new ArrayList<>();
             list.add(new Model(R.drawable.kilers,"Эрнест Хэмингуэй. Киллеры",R.string.kilers,"https://drive.google.com/open?id=1SWcocqAqJ_qa9xqLBBe5RloLW7YyoR0G"));
-            list.add(new Model(R.drawable.b,"Stec",R.string.to_be_to_be_be_am_m_are_is_iz,"https://drive.google.com/open?id=1jFbidxKvyUHpfrApgqT1XqXlEmVq7eYj"));
-            list.add(new Model(R.drawable.v,"hmara",R.string.to_be_to_be_be_am_m_are_is_iz," https://drive.google.com/drive/u/0/folders/1qqMtGlwX2s515N4WjqwTIL_9aYhSWCf9"));
-            list.add(new Model(R.drawable.c,"clara",R.string.to_be_to_be_be_am_m_are_is_iz,"https://drive.google.com/open?id=1SWcocqAqJ_qa9xqLBBe5RloLW7YyoR0G"));
+            list.add(new Model(R.drawable.the_third_man,"The Third Man (Третий человек)",R.string.The_Third_Man,"https://drive.google.com/open?id=1CmuofozQFj8G02RgKljcp-GExNWOTP6t"));
+            list.add(new Model(R.drawable.a_sound_of_thunder," Рэй Бредбери И грянул гром Ray Bradbury A Sound of Thunder",R.string.a_sound_of_thunder,"https://drive.google.com/open?id=16outXzpBroYtH4pdjAim5Ce9kF_-nG6Q"));
+            list.add(new Model(R.drawable.scazki,"ИРЛАНДСКИЕ ВОЛШЕБНЫЕ СКАЗКИ Irish Fairy Tales",R.string.Irish_Fairy_Tales,"https://drive.google.com/open?id=13QjbIrWaBWSHijavyAuInclzYSUhfm6K"));
+            list.add(new Model(R.drawable.besmaslesa,"Edward LEAR. A Book of Nonsense (Эдвард ЛИР. Книга бессмыслицы)",R.string.of_Nonsense,"https://drive.google.com/open?id=1dEMThCOWc2qdQ-seLOixIurshU_fmFld"));
+            list.add(new Model(R.drawable.socol,"Dashiell Hammett. The Maltese Falcon (Дэшил Хэммет. Мальтийский сокол)",R.string.socol,"https://drive.google.com/open?id=1bhstB3urysUCuIPG4EdJmzyFSBL2uO1c"));
+            list.add(new Model(R.drawable.ostrov,"Английский язык с Р.Л.Стивенсоном Остров сокровищ Robert Louis Stevenson Treasure Island",R.string.ostrov,"https://drive.google.com/open?id=1pDISoESI2AGtGx0w55KCd3xWnOELD8CU"));
+            list.add(new Model(R.drawable.robert,"Английский язык с Конаном Robert E.Howard Роберт Говард",R.string.Robert_E_Howard,"https://drive.google.com/open?id=1Ygq1yOi80opJxxXJ88cxmk1gHXyBvM2X"));
+            list.add(new Model(R.drawable.gandalf,"Английский язык с Мюриэль Спарк (рассказы) Портобелло Роуд The Portobello Road",R.string.The_Portobello,"https://drive.google.com/open?id=1w_MjrS6OtRw0Hg18XSLIuyRz8AvS3gmP"));
+            list.add(new Model(R.drawable.walliam,"Уильям Сомерсет Моэм Театр William Somerset Maugham Theatre",R.string.somerset,"https://drive.google.com/open?id=1w_MjrS6OtRw0Hg18XSLIuyRz8AvS3gmP"));
+            list.add(new Model(R.drawable.zpage,"Robinson Crusoe Written Anew for Children by James Baldwin",R.string.James,"https://drive.google.com/open?id=1Tvw134ijGeDJuJTP0FkUlcIZWD9tFK1f"));
 
             adaptor = new Adaptor(list,getContext(),this);
 
@@ -60,10 +67,16 @@ public class ListViewPager extends Fragment implements Listener {
             viewPager.setPadding(50, 0, 50, 0);
 
             Integer[] colors_temp = {
-                    getResources().getColor(R.color.color1),
-                    getResources().getColor(R.color.color2),
-                    getResources().getColor(R.color.color3),
-                    getResources().getColor(R.color.color4),
+                    getResources().getColor(R.color.color14),
+                    getResources().getColor(R.color.color23),
+                    getResources().getColor(R.color.color32),
+                    getResources().getColor(R.color.color41),
+                    getResources().getColor(R.color.color42),
+                    getResources().getColor(R.color.color43),
+                    getResources().getColor(R.color.color44),
+                    getResources().getColor(R.color.color45),
+                    getResources().getColor(R.color.color46),
+                    getResources().getColor(R.color.color47),
             };
 
             colors = colors_temp;
