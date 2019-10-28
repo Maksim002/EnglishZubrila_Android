f305392175467fac6a8847530b93c23aa54c5c9d
User <inoplanee@mail.ru>
2019-10-28 15:57:37 +0600

    new

---
+++ app/src/main/java/mainactivity/musicplayer/example/com/englishzubrila/corses/must/Must.java
@@ -0,0 +1,16 @@
+package mainactivity.musicplayer.example.com.englishzubrila.corses.must;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import mainactivity.musicplayer.example.com.englishzubrila.R;
+
+public class Must extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_must);
+    }
+}
